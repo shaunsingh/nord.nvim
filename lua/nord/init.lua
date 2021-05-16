daf1b6e424f2cda77191426a0ec8d34d237e6eb8
shaunsingh <71196912+shaunsingh@users.noreply.github.com>
2021-05-16 13:32:29 -0400

    Update init.lua

--- lua/nord/init.lua
+++ lua/nord/init.lua
@@ -1,3 +1,7 @@
+-- Colorscheme name:    nord.nvim
+-- Description:         Port of articicestudio's nord theme for neovim
+-- Author:              https://github.com/shaunsingh
+
 local util = require('nord.util')
 
 -- Load the theme
