0ef2671c433958aa1f5cd545a3fea439169ebc34
shaunsingh <shaunsingh0207@gmail.com>
2021-08-03 09:53:01 -0400

    Update lualine colors to reflect theme change

--- lua/lualine/themes/nord.lua
+++ lua/lualine/themes/nord.lua
@@ -3,35 +3,35 @@ local colors = require('nord.colors')
 local nord = {}
 
 nord.normal = {
-	a = {fg = colors.nord0_gui, bg = colors.accent, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
-	c = {fg = colors.fg, bg = colors.selection},
+	a = {fg = colors.nord0_gui, bg = colors.nord4_gui, gui = 'bold'},
+	b = {fg = colors.nord10_gui, bg = colors.nord10_gui},
+	c = {fg = colors.nord4_gui, bg = colors.nord1_gui},
 }
 
 nord.insert = {
 	a = {fg = colors.nord0_gui, bg = colors.nord14_gui, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+	b = {fg = colors.nord10_gui, bg = colors.nord10_gui},
 }
 
 nord.visual = {
 	a = {fg = colors.nord0_gui, bg = colors.nord9_gui, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+	b = {fg = colors.nord10_gui, bg = colors.nord10_gui},
 }
 
 nord.replace = {
 	a = {fg = colors.nord0_gui, bg = colors.nord11_gui, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+	b = {fg = colors.nord10_gui, bg = colors.nord10_gui},
 }
 
 nord.command = {
 	a = {fg = colors.nord0_gui, bg = colors.nord15_gui, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+	b = {fg = colors.nord10_gui, bg = colors.nord10_gui},
 }
 
-nord.inactive = {
-  a = {fg = colors.disabled, bg = colors.bg, gui = 'bold'},
-  b = {fg = colors.disabled, bg = colors.bg},
-  c = {fg = colors.disabled, bg = colors.selection}
+nord.innord10_gui = {
+  a = {fg = colors.nord1_gui, bg = colors.nord0_gui, gui = 'bold'},
+  b = {fg = colors.nord1_gui, bg = colors.nord0_gui},
+  c = {fg = colors.nord1_gui, bg = colors.nord1_gui}
 }
 
 return nord
