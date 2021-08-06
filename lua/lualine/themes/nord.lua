9256c8ebdf6599a64189db61180f75a5e9618ecc
S. Arjun <59405008+systemctl603@users.noreply.github.com>
2021-08-06 11:52:12 +0530

    Fix issue where lualine is not showing some sections
    Using lualine, sometimes the foreground color and the background color are the same, leading to some unreadable text.

--- lua/lualine/themes/nord.lua
+++ lua/lualine/themes/nord.lua
@@ -4,28 +4,28 @@ local nord = {}
 
 nord.normal = {
 	a = {fg = colors.nord0_gui, bg = colors.nord4_gui, gui = 'bold'},
-	b = {fg = colors.nord10_gui, bg = colors.nord10_gui},
+	b = {fg = colors.nord4_gui, bg = colors.nord10_gui},
 	c = {fg = colors.nord4_gui, bg = colors.nord1_gui},
 }
 
 nord.insert = {
 	a = {fg = colors.nord0_gui, bg = colors.nord14_gui, gui = 'bold'},
-	b = {fg = colors.nord10_gui, bg = colors.nord10_gui},
+	b = {fg = colors.nord4_gui, bg = colors.nord10_gui},
 }
 
 nord.visual = {
 	a = {fg = colors.nord0_gui, bg = colors.nord9_gui, gui = 'bold'},
-	b = {fg = colors.nord10_gui, bg = colors.nord10_gui},
+	b = {fg = colors.nord4_gui, bg = colors.nord10_gui},
 }
 
 nord.replace = {
 	a = {fg = colors.nord0_gui, bg = colors.nord11_gui, gui = 'bold'},
-	b = {fg = colors.nord10_gui, bg = colors.nord10_gui},
+	b = {fg = colors.nord4_gui, bg = colors.nord10_gui},
 }
 
 nord.command = {
 	a = {fg = colors.nord0_gui, bg = colors.nord15_gui, gui = 'bold'},
-	b = {fg = colors.nord10_gui, bg = colors.nord10_gui},
+	b = {fg = colors.nord4_gui, bg = colors.nord10_gui},
 }
 
 nord.innord10_gui = {
