1175b7ef9ce2aeff1d71ca753fbf1c149bfc0bbc
shaunsingh <shaunsingh0207@gmail.com>
2021-09-23 22:20:29 -0400

    Adjust ts-rainbow highlighting to use only Aurora Palette

--- lua/nord/theme.lua
+++ lua/nord/theme.lua
@@ -415,12 +415,12 @@ theme.loadPlugins = function()
 
     -- nvim-ts-rainbow
     rainbowcol1 = { fg = nord.nord15_gui },
-    rainbowcol2 = { fg = nord.nord14_gui },
-    rainbowcol3 = { fg = nord.nord13_gui },
+    rainbowcol2 = { fg = nord.nord13_gui },
+    rainbowcol3 = { fg = nord.nord14_gui },
     rainbowcol4 = { fg = nord.nord12_gui },
     rainbowcol5 = { fg = nord.nord11_gui },
-    rainbowcol6 = { fg = nord.nord10_gui },
-    rainbowcol7 = { fg = nord.nord9_gui },
+    rainbowcol6 = { fg = nord.nord15_gui },
+    rainbowcol7 = { fg = nord.nord13_gui },
 
     -- lightspeed
     LightspeedLabel = { fg = nord.nord8_gui, style = "bold" },
