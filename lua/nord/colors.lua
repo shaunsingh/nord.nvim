3f5a6ae1b77fa586fd03f5069fe1dd4f64e410a5
Khiem Ton That <29520244+th4tkh13m@users.noreply.github.com>
2021-11-04 09:00:17 +0700

    Edit color

--- lua/nord/colors.lua
+++ lua/nord/colors.lua
@@ -1,38 +1,62 @@
-local nord = {
+local onenord = {
   --16 colors
-  nord0_gui = "#2E3440"        ,
-  nord1_gui = "#3B4252"        ,
-  nord2_gui = "#434C5E"        ,
-  nord3_gui = "#4C566A"        ,
-  nord3_gui_bright = "#616E88" ,
-  nord4_gui = "#D8DEE9"        ,
-  nord5_gui = "#E5E9F0"        ,
-  nord6_gui = "#ECEFF4"        ,
-  nord7_gui = "#8FBCBB"        ,
-  nord8_gui = "#88C0D0"        ,
-  nord9_gui = "#81A1C1"        ,
-  nord10_gui = "#5E81AC"       ,
-  nord11_gui = "#BF616A"       ,
-  nord12_gui = "#D08770"       ,
-  nord13_gui = "#EBCB8B"       ,
-  nord14_gui = "#A3BE8C"       ,
-  nord15_gui = "#B48EAD"       ,
-  none =          'NONE'
+  fg = "#C8D0E0",
+  fg_light = "#E5E9F0",
+  bg = "#2E3440",
+  gray = "#646A76",
+  light_gray = "#6C7A96",
+  cyan = "#88C0D0",
+  blue = "#81A1C1",
+  dark_blue = "#5E81AC",
+  green = "#A3BE8C",
+  light_green = "#8FBCBB",
+  dark_red = "#BF616A",
+  red = "#D57780",
+  light_red = "#DE878F",
+  pink = "#E85B7A",
+  dark_pink = "#E44675",
+  orange = "#D08F70",
+  yellow = "#EBCB8B",
+  purple = "#B988B0",
+  light_purple = "#B48EAD",
+  none = "NONE",
 }
 
 -- Enable contrast sidebars, floating windows and popup menus
-if vim.g.nord_contrast == true then
-  nord.sidebar = nord.nord1_gui
-  nord.float = nord.nord1_gui
-else
-  nord.sidebar = nord.nord0_gui
-  nord.float = nord.nord0_gui
-end
+-- if vim.g.nord_contrast == true then
+--   nord.sidebar = nord.nord1_gui
+--   nord.float = nord.nord1_gui
+-- else
+--   nord.sidebar = nord.nord0_gui
+--   nord.float = nord.nord0_gui
+-- end
 
-if vim.g.nord_cursorline_transparent == true then
-  nord.cursorlinefg = nord.nord0_gui
-else
-  nord.cursorlinefg = nord.nord1_gui
-end
+-- if vim.g.nord_cursorline_transparent == true then
+--   nord.cursorlinefg = nord.nord0_gui
+-- else
+--   nord.cursorlinefg = nord.nord1_gui
+-- end
+
+-- more semantically meaningful colors
+
+onenord.error = onenord.dark_red
+onenord.warn = onenord.orange
+onenord.info = onenord.green
+onenord.hint = onenord.purple
+
+onenord.diff_add = onenord.green
+onenord.diff_add_bg = "#324125"
+onenord.diff_change = onenord.dark_blue
+onenord.diff_change_bg = "#334963"
+onenord.diff_remove = onenord.red
+onenord.diff_remove_bg = onenord.dark_red
+onenord.diff_text_bg = "#446285"
+
+onenord.active = "#353B49"
+onenord.float = "#3B4252"
+onenord.highlight = "#434C5E"
+onenord.selection = "#4C566A"
+
+return onenord
 
 return nord
