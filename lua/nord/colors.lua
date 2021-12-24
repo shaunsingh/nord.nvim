6046c1153a2cbdf3acd43100e038d52f62338adf
Slotos <slotos@gmail.com>
2021-12-24 02:20:12 +0200

    Implement naive Snow Storm palette
    This basically reverts first seven palette colors'
    order, which is what "bright to dark" style recommends.
    Bright color used for comments is blended by hand and
    should be critiqued.
    
    Dark to bright approach is also possible, by swapping
    nord0_gui and nord2_gui in light palette.
    However, the outcome looks incomplete and definitely needs
    additional work to be viable.
    
    Note: the colors might be hard to read on thin fonts
          I.e. Cascadia Mono Pl Light takes some adjusting
          while Cascadia Mono Pl looks good outright

--- lua/nord/colors.lua
+++ lua/nord/colors.lua
@@ -1,23 +1,43 @@
-local nord = {
+local nord = (vim.o.background == 'dark') and {
   --16 colors
-  nord0_gui = "#2E3440"        ,
-  nord1_gui = "#3B4252"        ,
-  nord2_gui = "#434C5E"        ,
-  nord3_gui = "#4C566A"        ,
-  nord3_gui_bright = "#616E88" ,
-  nord4_gui = "#D8DEE9"        ,
-  nord5_gui = "#E5E9F0"        ,
-  nord6_gui = "#ECEFF4"        ,
-  nord7_gui = "#8FBCBB"        ,
-  nord8_gui = "#88C0D0"        ,
-  nord9_gui = "#81A1C1"        ,
-  nord10_gui = "#5E81AC"       ,
-  nord11_gui = "#BF616A"       ,
-  nord12_gui = "#D08770"       ,
-  nord13_gui = "#EBCB8B"       ,
-  nord14_gui = "#A3BE8C"       ,
-  nord15_gui = "#B48EAD"       ,
-  none =          'NONE'
+  nord0_gui        = "#2E3440", -- nord0 in palette
+  nord1_gui        = "#3B4252", -- nord1 in palette
+  nord2_gui        = "#434C5E", -- nord2 in palette
+  nord3_gui        = "#4C566A", -- nord3 in palette
+  nord3_gui_bright = "#616E88", -- out of palette
+  nord4_gui        = "#D8DEE9", -- nord4 in palette
+  nord5_gui        = "#E5E9F0", -- nord5 in palette
+  nord6_gui        = "#ECEFF4", -- nord6 in palette
+  nord7_gui        = "#8FBCBB", -- nord7 in palette
+  nord8_gui        = "#88C0D0", -- nord8 in palette
+  nord9_gui        = "#81A1C1", -- nord9 in palette
+  nord10_gui       = "#5E81AC", -- nord10 in palette
+  nord11_gui       = "#BF616A", -- nord11 in palette
+  nord12_gui       = "#D08770", -- nord12 in palette
+  nord13_gui       = "#EBCB8B", -- nord13 in palette
+  nord14_gui       = "#A3BE8C", -- nord14 in palette
+  nord15_gui       = "#B48EAD", -- nord15 in palette
+  none             = 'NONE'
+} or {
+  --16 colors
+  nord0_gui        = "#ECEFF4", -- nord6 in palette
+  nord1_gui        = "#E5E9F0", -- nord5 in palette
+  nord2_gui        = "#D8DEE9", -- nord4 in palette
+  nord3_gui        = "#4C566A", -- out of palette
+  nord3_gui_bright = "#AEC7DF", -- nord3 in palette
+  nord4_gui        = "#434C5E", -- nord2 in palette
+  nord5_gui        = "#3B4252", -- nord1 in palette
+  nord6_gui        = "#2E3440", -- nord0 in palette
+  nord7_gui        = "#8FBCBB", -- nord7 in palette
+  nord8_gui        = "#88C0D0", -- nord8 in palette
+  nord9_gui        = "#81A1C1", -- nord9 in palette
+  nord10_gui       = "#5E81AC", -- nord10 in palette
+  nord11_gui       = "#BF616A", -- nord11 in palette
+  nord12_gui       = "#D08770", -- nord12 in palette
+  nord13_gui       = "#EBCB8B", -- nord13 in palette
+  nord14_gui       = "#A3BE8C", -- nord14 in palette
+  nord15_gui       = "#B48EAD", -- nord15 in palette
+  none             = 'NONE'
 }
 
 -- Enable contrast sidebars, floating windows and popup menus
--- lua/nord/util.lua
+++ lua/nord/util.lua
@@ -38,7 +38,7 @@ function util.load()
   -- Set the theme environment
   vim.cmd("hi clear")
   if vim.fn.exists("syntax_on") then vim.cmd("syntax reset") end
-  vim.o.background = "dark"
+  -- vim.o.background = "dark"
   vim.o.termguicolors = true
   vim.g.colors_name = "nord"
 
